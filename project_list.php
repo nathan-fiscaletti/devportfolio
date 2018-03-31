d4a4ff62b9afdab14a3d1c10f329230e436118e7
Nathan Fiscaletti <nathan.fiscaletti@gmail.com>
2018-03-30 22:09:34 -0500

    Updated project_list.php

--- project_list.php
+++ project_list.php
@@ -1,34 +1,38 @@
 <?php
 
-return [
-    0 => [
-        'name' => 'Synful',
-        'description' => 'A versatile PHP framework for you to create your own API with ease.',
-        'http' => 'https://git.nathanf.tk/synful/wiki',
-        'image' => 'images/SynfulLogo.jpg',
-    ],
-    1 => [
-        'name' => 'Extended Arrays',
-        'description' => 'A PHP library offering improved array management.',
-        'http' => 'http://git.nathanf.tk/extended-arrays',
-        'image' => 'images/ExtendedArrayLogo.jpg',
-    ],
-    2 => [
-        'name' => 'JTwoD',
-        'description' => 'A 2D Java game engine written on top of AWT.',
-        'http' => 'http://git.nathanf.tk/jtwod',
-        'image' => 'images/JtwoDLogo.jpg',
-    ],
-    3 => [
-        'name' => 'Parameter-Parser',
-        'description' => 'A PHP library for parsing command line parameters.',
-        'http' => 'http://git.nathanf.tk/parameterparser',
-        'image' => 'images/ParameterParserLogo.png',
-    ],
-    4 => [
-        'name' => 'FreeGeoIp for PHP',
-        'description' => 'FreeGeoIp for PHP is a simple library used to interface with any freegeoip API.',
-        'http' => 'http://git.nathanf.tk/freegeoip-php',
-        'image' => 'images/FreeGeoIpPhpLogo.jpg',
-    ],
-];
+if ( basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"]) ) { 
+    header("Location: ./");
+} else {
+    return [
+        0 => [
+            'name' => 'Synful',
+            'description' => 'A versatile PHP framework for you to create your own API with ease.',
+            'http' => 'https://git.nathanf.tk/synful/wiki',
+            'image' => 'images/SynfulLogo.jpg',
+        ],
+        1 => [
+            'name' => 'Extended Arrays',
+            'description' => 'A PHP library offering improved array management.',
+            'http' => 'http://git.nathanf.tk/extended-arrays',
+            'image' => 'images/ExtendedArrayLogo.jpg',
+        ],
+        2 => [
+            'name' => 'JTwoD',
+            'description' => 'A 2D Java game engine written on top of AWT.',
+            'http' => 'http://git.nathanf.tk/jtwod',
+            'image' => 'images/JtwoDLogo.jpg',
+        ],
+        3 => [
+            'name' => 'Parameter-Parser',
+            'description' => 'A PHP library for parsing command line parameters.',
+            'http' => 'http://git.nathanf.tk/parameterparser',
+            'image' => 'images/ParameterParserLogo.png',
+        ],
+        4 => [
+            'name' => 'FreeGeoIp for PHP',
+            'description' => 'FreeGeoIp for PHP is a simple library used to interface with any freegeoip API.',
+            'http' => 'http://git.nathanf.tk/freegeoip-php',
+            'image' => 'images/FreeGeoIpPhpLogo.jpg',
+        ],
+    ];
+}
